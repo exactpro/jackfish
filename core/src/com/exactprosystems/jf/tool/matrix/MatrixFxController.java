c9b60faedd021e5c6ad3e7655b200298de8f96f1
andrey.bystrov <andrey.bystrov@exactprosystems.com>
2016-01-18 09:33:30 +0000

    1. replaced display info about breakpoint on tab of matrix 2. add execute testcase on matrix allControlsAndOperations.jf

--- core/src/com/exactprosystems/jf/tool/matrix/MatrixFxController.java
+++ core/src/com/exactprosystems/jf/tool/matrix/MatrixFxController.java
@@ -266,7 +266,8 @@ public class MatrixFxController implements Initializable, ContainingParent, IMat
 		Optional.ofNullable(this.watcher).ifPresent(WatcherFx::update);
 		TreeItem<MatrixItem> treeItem = this.tree.find(item);
 		((GridPane) treeItem.getValue().getLayout()).getStyleClass().add(CssVariables.PAUSED_MATRIX_ITEM);
-		listView.getItems().add(ConsoleText.pausedItem(String.format("Matrix paused on \'%s\'", treeItem.getValue().getItemName()), item));
+		DialogsHelper.showInfo(String.format("Matrix paused on \'%s\'", treeItem.getValue().getItemName()));
+		this.tree.scrollTo(this.tree.getRow(treeItem));
 	}
 
 	@Override
