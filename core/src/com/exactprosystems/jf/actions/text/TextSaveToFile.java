b71bf77318daf2172d458e2b4394f51530a5920d
victor.krasnovid <victor.krasnovid@exactprosystems.com>
2016-11-21 20:01:55 +0400

    Correct a buit-in documentation in text actions

--- core/src/com/exactprosystems/jf/actions/text/TextSaveToFile.java
+++ core/src/com/exactprosystems/jf/actions/text/TextSaveToFile.java
@@ -23,7 +23,7 @@ import com.exactprosystems.jf.functions.Text;
 		group					= ActionGroups.Text,
 		generalDescription 		=
  "The purpose of the action is to save object {{$Text$}} to a file. "
-+ "The object type Text is the text-based pattern which consists of lines.",
++ "The object type {{$Text$}} is the text-based pattern which consists of lines.",
 		additionFieldsAllowed 	= false,
 		outputDescription 		= "True if saving is successfull.",
 		outputType				= Boolean.class,
@@ -45,7 +45,7 @@ public class TextSaveToFile extends AbstractAction
 	public final static String textName = "Text";
 	public final static String fileNameName = "File";
 
-	@ActionFieldAttribute(name = textName, mandatory = true, description = "Text(Text) - object Text,which is required to output.")
+	@ActionFieldAttribute(name = textName, mandatory = true, description = "Text(Text) - object {{$Text$}},which is required to output.")
 	protected Text 	text 	= null;
 
 	@ActionFieldAttribute(name = fileNameName, mandatory = true, description = "File(String) - a path where to save a file and the file name with an extension.")
