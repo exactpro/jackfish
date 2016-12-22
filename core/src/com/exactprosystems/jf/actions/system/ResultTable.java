02cb57fc6248aebeda857f0e11e23647ce77b3fd
victor.krasnovid <victor.krasnovid@exactprosystems.com>
2016-12-22 17:39:25 +0400

    RM38539_regrerssion_framework_small fix

--- core/src/com/exactprosystems/jf/actions/system/ResultTable.java
+++ core/src/com/exactprosystems/jf/actions/system/ResultTable.java
@@ -106,7 +106,7 @@ public class ResultTable extends AbstractAction
 		}
 		
 		Table copy = result.clone();
-		copy.setValue(copy.size() - 1, map);
+//		copy.setValue(copy.size() - 1, map);		TODO zzz
 		if (this.decorated)
 		{
 			String passed = report.decorateStyle(Result.Passed.name(), Result.Passed.getStyle());
