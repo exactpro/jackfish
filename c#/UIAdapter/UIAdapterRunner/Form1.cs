dbeb3de9899cf4714b20a1f0d854763b7d77593b
andrey.smirnov <andrey.smirnov@exactprosystems.com>
2017-03-22 19:52:18 +0300

    add adapterRunner

---
+++ c#/UIAdapter/UIAdapterRunner/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UIAdapterRunner
+{
+    public partial class UIAdapterRunner : Form
+    {
+        public UIAdapterRunner()
+        {
+            InitializeComponent();
+            //UIAdapter.Program.Run("C:/Users/user_adm/workspace - git/JackFish/TestProject/../mocks/win/mockWin.exe", "", "");
+        }
+    }
+}
