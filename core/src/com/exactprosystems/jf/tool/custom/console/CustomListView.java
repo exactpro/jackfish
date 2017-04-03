e1044818182613425cb90def0179d1730aceb5e8
victor.krasnovid <victor.krasnovid@exactprosystems.com>
2017-04-03 14:46:32 +0400

    RM 39455 multiply select and copy from JF console added

--- core/src/com/exactprosystems/jf/tool/custom/console/CustomListView.java
+++ core/src/com/exactprosystems/jf/tool/custom/console/CustomListView.java
@@ -8,61 +8,92 @@
 
 package com.exactprosystems.jf.tool.custom.console;
 
+import com.exactprosystems.jf.api.common.Sys;
 import javafx.application.Platform;
 import javafx.collections.ListChangeListener;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.SelectionMode;
 
-public class CustomListView<T> extends ListView<ConsoleText<T>>
-{
-	private OnDoubleClickListener<T> listener;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
-	public CustomListView(final boolean mayClear)
-	{
-		super();
-		this.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-		this.setCellFactory(cellConsoleFxListView -> new ColorCell<>(mayClear));
+public class CustomListView<T> extends ListView<ConsoleText<T>> {
+    private OnDoubleClickListener<T> listener;
 
-		this.setOnMouseClicked(mouseEvent -> {
-			if (mouseEvent.getClickCount() == 2)
-			{
-				ConsoleText<T> selectedItem = getSelectionModel().getSelectedItem();
-				if (selectedItem != null && listener != null && selectedItem.getItem() != null)
-				{
-					listener.onDoubleClick(selectedItem.getItem());
-				}
-			}
-		});
-	}
+    public CustomListView(final boolean mayClear) {
+        super();
+        this.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        this.setCellFactory(cellConsoleFxListView -> new ColorCell<>());
 
-	public CustomListView(OnDoubleClickListener<T> listener, boolean mayClear)
-	{
-		this(mayClear);
-		this.listener = listener;
-	}
+        this.setOnMouseClicked(mouseEvent ->
+        {
+            if (mouseEvent.getClickCount() == 2)
+            {
+                ConsoleText<T> selectedItem = getSelectionModel().getSelectedItem();
+                if (selectedItem != null && listener != null && selectedItem.getItem() != null)
+                {
+                    listener.onDoubleClick(selectedItem.getItem());
+                }
+            }
+        });
 
-	public void autoScroll(boolean flag)
-	{
-		if (flag)
-		{
-			this.getItems().addListener(changeListener);
-		}
-		else
-		{
-			this.getItems().removeListener(changeListener);
-		}
-	}
+        ContextMenu contextMenu = new ContextMenu();
 
-	public void setListener(OnDoubleClickListener<T> listener)
-	{
-		this.listener = listener;
-	}
+        MenuItem clear = new MenuItem("Clear");
+        MenuItem copy = new MenuItem("Copy");
 
-	private ListChangeListener<ConsoleText<T>> changeListener = c -> {
-		int size = this.getItems().size();
-		if (size > 0)
-		{
-			Platform.runLater(() -> this.scrollTo(size - 1));
-		}
-	};
+        contextMenu.getItems().addAll(copy);
+        if (mayClear)
+        {
+            contextMenu.getItems().add(clear);
+        }
+
+        setContextMenu(contextMenu);
+
+        clear.setOnAction(event ->
+        {
+            this.getItems().forEach(i -> i.setText(""));
+            this.getItems().clear();
+        });
+
+        copy.setOnAction(event -> Sys.copyToClipboard(
+                this.getSelectionModel().getSelectedItems()
+                        .stream()
+                        .filter(Objects::nonNull)
+                        .map(ConsoleText::toString)
+                        .collect(Collectors.joining("\n"))
+                )
+        );
+    }
+
+    public CustomListView(OnDoubleClickListener<T> listener, boolean mayClear) {
+        this(mayClear);
+        this.listener = listener;
+    }
+
+    public void autoScroll(boolean flag) {
+        if (flag)
+        {
+            this.getItems().addListener(changeListener);
+        }
+        else
+        {
+            this.getItems().removeListener(changeListener);
+        }
+    }
+
+    public void setListener(OnDoubleClickListener<T> listener) {
+        this.listener = listener;
+    }
+
+    private ListChangeListener<ConsoleText<T>> changeListener = c ->
+    {
+        int size = this.getItems().size();
+        if (size > 0)
+        {
+            Platform.runLater(() -> this.scrollTo(size - 1));
+        }
+    };
 }
