cae9fe66f055f933e87e683cab7e63a34ea67e14
alexander.kruglov <alexander.kruglov@exactprosystems.com>
2017-05-29 13:29:14 +0400

    TexDocs(font size)

--- core/src/com/exactprosystems/jf/common/documentation/DocumentationBuilder.java
+++ core/src/com/exactprosystems/jf/common/documentation/DocumentationBuilder.java
@@ -78,13 +78,13 @@ public class DocumentationBuilder
         addTable(help, "", table3, new int[] { 50, 50 },  evaluator);
 
         //todo table of contents
-//        addChapter(help, "", 4);
-//        addChapter(help, " {{toc}}", 4);
+        addChapter(help, "", 4);
+        addChapter(help, "\\tableofcontents", 4);
 
         addChapter(help, "", 4);
         addText(help, DocumentationBuilder.class.getResourceAsStream("intro1.txt"));
         addPicture(help, "Architecture", DocumentationBuilder.class.getResourceAsStream("Intro.png"));
-//        addText(help, DocumentationBuilder.class.getResourceAsStream("intro2.txt"));
+        addText(help, DocumentationBuilder.class.getResourceAsStream("intro2.txt"));
 //        addChapter(help, "MVEL", 3);
 //        addText(help, DocumentationBuilder.class.getResourceAsStream("mvel.txt"));
 //        addChapter(help, "All controls", 3);
