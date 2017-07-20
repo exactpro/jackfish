91b6380525931ecaeb121828b63dfbd5c19a53ae
victor.krasnovid <victor.krasnovid@exactprosystems.com>
2017-07-20 16:43:52 +0400

    RM42866. 	Memory shortage problem for more cycle with steps and enabled screenshot

--- api/src/com/exactprosystems/jf/api/app/ImageWrapper.java
+++ api/src/com/exactprosystems/jf/api/app/ImageWrapper.java
@@ -11,12 +11,7 @@ package com.exactprosystems.jf.api.app;
 import javax.imageio.ImageIO;
 import com.exactprosystems.jf.api.common.Storable;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.util.Arrays;
 import java.util.List;
 
@@ -26,27 +21,27 @@ public class ImageWrapper implements Storable
 
 	private int					width;
 	private int					height;
-	private int[]				pixels;
+	private byte[]				bytes;
 
 	public ImageWrapper()
 	{
 		this.width = 1;
 		this.height = 1;
-		this.pixels = new int[] {0};
+		this.bytes = new byte[0];
 	}
 
 	public ImageWrapper(int width, int height, int[] buffer)
 	{
 		this.width = width;
 		this.height = height;
-		this.pixels = buffer;
+		this.bytes = intToBytes(buffer);
 	}
 
 	public ImageWrapper(BufferedImage bi)
 	{
 		this.width = bi.getWidth();
 		this.height = bi.getHeight();
-		this.pixels = bi.getRGB(0, 0, bi.getWidth(), bi.getHeight(), null, 0, bi.getWidth());
+		this.bytes = fileToBytes(bi);
 	}
 	
 	@Override
@@ -82,10 +77,9 @@ public class ImageWrapper implements Storable
 	}
 
 	@Override
-	public byte[] getData(String file) throws IOException 
+	public byte[] getData(String file) throws IOException
 	{
-		try (ByteArrayOutputStream baos = new ByteArrayOutputStream(); 
-				ByteArrayOutputStream temp = new ByteArrayOutputStream())
+		try (ByteArrayOutputStream temp = new ByteArrayOutputStream())
 		{
 			ImageIO.write(getImage(), "jpg", temp);
 			return temp.toByteArray();
@@ -96,27 +90,27 @@ public class ImageWrapper implements Storable
 	public void addFile(String file, byte[] data) throws IOException 
 	{
 		setDescription(file);
+		this.bytes = data;
 		
 		try (ByteArrayInputStream bais = new ByteArrayInputStream(data))
 		{
 			BufferedImage bi = ImageIO.read(bais);
 			this.width = bi.getWidth();
 			this.height = bi.getHeight();
-			this.pixels = bi.getRGB(0, 0, bi.getWidth(), bi.getHeight(), null, 0, bi.getWidth());
 		}
 	}
 
 	public ImageWrapper cutImage(int x1, int y1, int x2, int y2)
 	{
 		BufferedImage bi = new BufferedImage(this.width, this.height, BufferedImage.TYPE_INT_RGB);
-		bi.setRGB(0, 0, this.width, this.height, this.pixels, 0, this.width);
+		bi.setRGB(0, 0, this.width, this.height, bytesToIntArray(), 0, this.width);
 		return new ImageWrapper(bi.getSubimage(x1, y1, x2-x1, y1-y2));
 	}
 
 	public BufferedImage getImage()
 	{
 		BufferedImage bi = new BufferedImage(this.width, this.height, BufferedImage.TYPE_INT_RGB);
-		bi.setRGB(0, 0, this.width, this.height, this.pixels, 0, this.width);
+		bi.setRGB(0, 0, this.width, this.height, bytesToIntArray() , 0, this.width);
 		return bi;
 	}
 
@@ -178,7 +172,49 @@ public class ImageWrapper implements Storable
 	{
 		this.description = description;
 	}
-	
+
+	private int[] bytesToIntArray() {
+
+		BufferedImage bi = bytesToImage(this.bytes);
+		return bi.getRGB(0, 0, bi.getWidth(), bi.getHeight(), null, 0, bi.getWidth());
+	}
+
+	private byte[] fileToBytes(BufferedImage bi) {
+
+		byte[] res = new byte[0];
+		try(ByteArrayOutputStream baos = new ByteArrayOutputStream())
+		{
+			ImageIO.write(bi, "jpg", baos);
+			baos.flush();
+			res = baos.toByteArray();
+		}
+		catch (IOException e)
+		{
+			//todo logger
+		}
+		return res;
+	}
+
+	private byte[] intToBytes(int[] ints) {
+		BufferedImage bi = new BufferedImage(this.width, this.height, BufferedImage.TYPE_INT_RGB);
+		bi.setRGB(0, 0, this.width, this.height, ints, 0, this.width);
+
+		return fileToBytes(bi);
+	}
+
+	private BufferedImage bytesToImage(byte[] bytes) {
+		BufferedImage res = null;
+
+		try (InputStream in = new ByteArrayInputStream(bytes)){
+			res = ImageIO.read(in);
+		}
+		catch (IOException e)
+		{
+			//todo logger
+		}
+
+		return res;
+	}
 	
 	private String	fileName	= null;
 	private String 	description	= null;
