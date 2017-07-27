eb908f4ace4787406851eed88a00fc3edb862e8d
victor.krasnovid <victor.krasnovid@exactprosystems.com>
2017-07-27 18:08:51 +0400

    FullText search in help improved

--- core/resources/com/exactprosystems/jf/common/report/help.js
+++ core/resources/com/exactprosystems/jf/common/report/help.js
@@ -66,149 +66,128 @@ $(document).ready(function () {
 
 		}
 
-	})
-
-
-	var f = function(){
-
-		var currentIndex = -1;
-
-
-		var foundDivs = [];
-
-
-		var scrollToDiv = function(div) {
-
-			$('html, body').animate({
-
-				scrollTop: $(div).offset().top-50
-
-			}, 500);
-
-		}
-
-		var scrollToElem = function(elem) {
-
-			$('html, body').animate({
-
-				scrollTop: $(elem).offset().top-50
-
-			}, 100);
-
-		}
-
-
-		jQuery.expr[":"].Contains = jQuery.expr.createPseudo(function(arg) {
-
-			return function( elem ) {
-
-				return jQuery(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
-
-			};
-
-		});
-
-
-
-		$(".searchInput").on("change paste", function(e) {
-
-                    $(".searchLabel").text(e.keyCode);
-        			var str = $(this).val();
-        			if (str.length === 0) {
-        				foundDivs = [];
-        				$(".searchLabel").text("");
-        				$("#btnPrev").removeClass('activeBtn');
-        				$("#btnPrev").addClass('noNactiveBtn');
-        				$("#btnNext").removeClass('activeBtn');
-        				$("#btnNext").addClass('noNactiveBtn');
-
-        			} else {
-        				foundDivs = $('h2:Contains("'+str+'"), p:Contains("'+str+'"), td:Contains("'+str+'")');
-        				var newDivs = [];
-        				for(var i = 0; i < foundDivs.length; i++) {
-        					var current = foundDivs[i];
-        					var flag = true;
-        					for(var j = 0; j < foundDivs.length; j++) {
-        						if (i == j) {
-        							continue;
-        						}
-        						if ($(current).has($(foundDivs[j])).length === 1) {
-        							flag = false;
-        							break;
-        						}
-        					}
-
-        					if (flag) {
-        						newDivs.push(current);
-        					}
-        				}
-        				foundDivs = newDivs;
-        			        }
-        				$('ins').contents().unwrap();
-        				currentIndex = 0;
-        				scrollToElem(foundDivs[0]);
-        			$(foundDivs).each(function() {
-        				var str = $('.searchInput').val();
-        				var re = new RegExp('(' + str + ')', 'ig');
-        				this.innerHTML = this.innerHTML.replace(re, '<ins>$1</ins>');
-        			});
-        			var allIns = $('ins');
-        				$(".searchLabel").text(allIns.length + " matches");
-
-        				$("#btnPrev").removeClass('noNactiveBtn');
-        				$("#btnPrev").addClass('activeBtn');
-        				$("#btnNext").removeClass('noNactiveBtn');
-        				$("#btnNext").addClass('activeBtn');
-        				$(allIns[0]).addClass('currentFoundElement');
-        				$(".searchLabel").text("1 of " + allIns.length);
-        		});
-
-		$("#btnNext").click(function(e) {
-			var allIns = $('ins');
-			if (foundDivs.length !== 0) {
-				currentIndex++;
-				if (currentIndex === allIns.length) {
-					currentIndex = 0;
-				}
-					$(allIns).each(function(){
-						$(this).removeClass('currentFoundElement')});
-				$(allIns[currentIndex]).addClass('currentFoundElement');
-				$(".searchLabel").text(currentIndex+1 + "        of " + allIns.length);
-				scrollToElem(allIns[currentIndex]);
-			}
-
-
-		});
-
-
-
-		$("#btnPrev").click(function(e) {
-			var allIns = $('ins');
-			if (foundDivs.length !== 0) {
-				currentIndex--;
-				if (currentIndex < 0) {
-					currentIndex = allIns.length -1;
-				}
-
-				$(allIns).each(function(){
-						$(this).removeClass('currentFoundElement')});
-
-				$(".searchLabel").text(currentIndex +1 + "     of     " + allIns.length);
-				$(allIns[currentIndex]).addClass('currentFoundElement');
-				$(".searchLabel").text(currentIndex +1 + "        of " + allIns.length);
-				scrollToElem(allIns[currentIndex]);
-
-			}
-
-
-		});
-
-
-	};
-
+	});
 	f();
-
 });
+
+function f(){
+var str = '';
+var currentIndex = 0;
+var allIns = [];
+var foundDivs = [];
+var scrollToDiv = function(div) {
+	$('html, body').animate({
+		scrollTop: $(div).offset().top-50
+	}, 500);
+}
+
+var scrollToElem = function(elem) {
+	$('html, body').animate({
+		scrollTop: $(elem).offset().top-50
+	}, 100);
+}
+
+jQuery.expr[":"].Contains = jQuery.expr.createPseudo(function(arg) {
+    return function( elem ) {
+		return jQuery(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
+    };
+});
+
+
+//go forward
+function goF() {
+    if(foundDivs.length === 0){return}
+    $(allIns[currentIndex]).toggleClass('currentFoundElement foundElement');
+    if(currentIndex+1 === allIns.length){
+        currentIndex = 0;
+        scrollToElem(allIns[currentIndex]);
+    }else{
+        scrollToElem(allIns[++ currentIndex])
+    }
+    $('.searchLabel').text(1 + currentIndex +' of '+ allIns.length);
+    $(allIns[currentIndex]).toggleClass('currentFoundElement foundElement');
+}
+
+//go back
+function goB() {
+    if(foundDivs.length === 0){return}
+    $(allIns[currentIndex]).toggleClass('currentFoundElement foundElement');
+    if(currentIndex === 0){
+        currentIndex = allIns.length-1;
+        scrollToElem(allIns[currentIndex]);
+    }else{
+        scrollToElem(allIns[--currentIndex]);
+    }
+    $('.searchLabel').text(1+currentIndex +' of '+ allIns.length);
+    $(allIns[currentIndex]).toggleClass('currentFoundElement foundElement');
+}
+
+function markAllElements(array, str) {
+    var re = new RegExp('(' + str + ')', 'ig');
+    $(array).each(function(i,elem){
+        elem.innerHTML = this.innerHTML.replace(re, '<ins>$1</ins>');
+    });
+    allIns = $('ins');
+    $('.searchLabel').text(allIns.length);
+    scrollToElem(allIns[0]);
+    currentIndex++;
+    $(allIns[0]).toggleClass('currentFoundElement');
+
+}
+
+function clear() {
+
+    currentIndex = -1;
+    $('ins').contents().unwrap();
+    foundDivs = [];
+    allIns = [];
+    $('.searchLabel').text('');
+    $("#btnPrev").toggleClass('activeBtn nonActiveBtn');
+    $("#btnNext").toggleClass('activeBtn nonActiveBtn');
+}
+
+
+$(".searchInput").on("keypress paste", function(e) {
+
+if(e.which == 13) {
+
+	if( $(this).val() === str){
+	    goF();
+	}else{
+
+    clear();
+    str = $(this).val();
+    if(str.includes(' | ')){str = str.replace('|','\\|')}
+    if(str.includes(' ^ ')){str = str.replace('^','\\^')}
+
+    if(str.length === 0){
+        clear();
+        return;
+    }
+
+    $("#btnPrev").toggleClass('activeBtn nonActiveBtn');
+    $("#btnNext").toggleClass('activeBtn nonActiveBtn');
+
+    if($(this).val().includes('|') || $(this).val().includes('^')){
+    	foundDivs = $('h2:contains('+str+'), p:contains('+str+'), td:contains('+str+'), font:contains('+str+'), pre:contains('+str+')');
+    }else{
+    	foundDivs = $('h2:Contains('+str+'), p:Contains('+str+'), td:Contains('+str+'), font:Contains('+str+'), pre:Contains('+str+')');
+    }
+
+    if(foundDivs.length === 0){
+        $('.searchLabel').text(0);
+        return;
+    }
+    markAllElements(foundDivs, str);
+    }
+
+    }
+});
+
+$("#btnNext").click(function(e){goF();});
+
+$("#btnPrev").click(function(e){goB();});
+}
 
 $(window).resize(function () {
 
