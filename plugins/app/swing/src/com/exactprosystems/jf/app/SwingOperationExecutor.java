e17a8b91a70559b66260307b31d8819ce284297a
alexander.kruglov <alexander.kruglov@exactprosystems.com>
2017-08-02 15:07:55 +0400

    RM43166 SWING working with Keyboard.CONTROL

--- plugins/app/swing/src/com/exactprosystems/jf/app/SwingOperationExecutor.java
+++ plugins/app/swing/src/com/exactprosystems/jf/app/SwingOperationExecutor.java
@@ -381,17 +381,27 @@ public class SwingOperationExecutor implements OperationExecutor<ComponentFixtur
 					this.isControlDown = b;
 					break;
 				default:
-					throw new OperationNotAllowedException(String.format("Component %s dosen't support press operation", component));
+					break;
 			}
 
 			final Component target = component.target;
 			final ArrayList<InputEvent> events = new ArrayList<>();
 			int keyCode = getKeyCode(key);
 			int modifiers = getModifierKeysArePressed();
+			boolean needPress = (keyCode != getKeyCode(Keyboard.CONTROL) && keyCode != getKeyCode(Keyboard.SHIFT) && keyCode != getKeyCode(Keyboard.ALT) && keyCode != getKeyCode(Keyboard.CAPS_LOCK));
 
 			if(b)
 			{
 				events.add(new KeyEvent(target, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), modifiers, keyCode, (char) keyCode));
+
+				if(needPress)
+				{
+					if (this.isControlDown){
+						events.add(new KeyEvent(target, KeyEvent.KEY_TYPED, System.currentTimeMillis(), modifiers, KeyEvent.VK_UNDEFINED, (char) getKeyCodeWithControl(key)));
+					} else {
+						events.add(new KeyEvent(target, KeyEvent.KEY_TYPED, System.currentTimeMillis(), modifiers, KeyEvent.VK_UNDEFINED, (char) keyCode));
+					}
+				}
 			}
 			else
 			{
@@ -410,10 +420,6 @@ public class SwingOperationExecutor implements OperationExecutor<ComponentFixtur
 					}
 				}
 			});
-		}
-		catch (RemoteException e)
-		{
-			throw e;
 		}
 		catch (Throwable e)
 		{
@@ -2295,6 +2301,43 @@ public class SwingOperationExecutor implements OperationExecutor<ComponentFixtur
 		return new ComponentFixture<T>(this.currentRobot, component)
 		{
 		};
+	}
+
+	//http://www.aivosto.com/vbtips/control-characters.html#list_C0
+	private int getKeyCodeWithControl(Keyboard key){
+		switch (key)
+		{
+			case A: return 1;
+			case B: return 2;
+			case C: return 3;
+			case D: return 4;
+			case E: return 5;
+			case F: return 6;
+			case G: return 7;
+			case H: return 8;
+			case I: return 9;
+			case J: return 10;
+			case K: return 11;
+			case L: return 12;
+			case M: return 13;
+			case N: return 14;
+			case O: return 15;
+			case P: return 16;
+			case Q: return 17;
+			case R: return 18;
+			case S: return 19;
+			case T: return 20;
+			case U: return 21;
+			case V: return 22;
+			case W: return 23;
+			case X: return 24;
+			case Y: return 25;
+			case Z: return 26;
+			case SPACE: return 32;
+			case DELETE: return 127;
+			default:
+				return getKeyCode(key);
+		}
 	}
 
 }
