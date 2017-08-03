6c3f878275227ed283681b579ee45b99706c563a
victor.krasnovid <victor.krasnovid@exactprosystems.com>
2017-08-03 18:02:28 +0400

    RM43773 intermediate update

--- core/src/com/exactprosystems/jf/tool/wizard/all/DialogFillWizard.java
+++ core/src/com/exactprosystems/jf/tool/wizard/all/DialogFillWizard.java
@@ -1,6 +1,7 @@
 package com.exactprosystems.jf.tool.wizard.all;
 
 import com.exactprosystems.jf.api.app.AppConnection;
+import com.exactprosystems.jf.api.app.IControl;
 import com.exactprosystems.jf.api.app.IWindow;
 import com.exactprosystems.jf.api.common.IContext;
 import com.exactprosystems.jf.api.wizard.WizardAttribute;
@@ -8,6 +9,7 @@ import com.exactprosystems.jf.api.wizard.WizardCategory;
 import com.exactprosystems.jf.api.wizard.WizardCommand;
 import com.exactprosystems.jf.api.wizard.WizardManager;
 import com.exactprosystems.jf.documents.config.Context;
+import com.exactprosystems.jf.documents.guidic.Window;
 import com.exactprosystems.jf.documents.matrix.parser.items.MatrixItem;
 import com.exactprosystems.jf.tool.ApplicationConnector;
 import com.exactprosystems.jf.tool.Common;
@@ -17,9 +19,12 @@ import com.exactprosystems.jf.tool.matrix.MatrixFx;
 import com.exactprosystems.jf.tool.wizard.AbstractWizard;
 import com.exactprosystems.jf.tool.wizard.CommandBuilder;
 import javafx.application.Platform;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.SingleSelectionModel;
+import javafx.geometry.HPos;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 
 import java.util.*;
 import java.util.function.Supplier;
@@ -43,6 +48,7 @@ public class DialogFillWizard extends AbstractWizard {
     private DictionaryFx dictionary;
     private ComboBox<String> storedConnections;
     private ComboBox<String> dialogs;
+    private List<IControl> controls;
     private String currentAdapterStore;
     private ApplicationConnector appConnector;
 
@@ -55,11 +61,17 @@ public class DialogFillWizard extends AbstractWizard {
         this.appConnector = new ApplicationConnector(((Context) context).getFactory());
 
 
-
     }
 
     @Override
     protected void initDialog(BorderPane borderPane) {
+
+        borderPane.prefHeight(600);
+        borderPane.minHeight(600);
+        borderPane.maxHeight(600);
+        borderPane.prefWidth(600);
+        borderPane.maxWidth(600);
+        borderPane.minWidth(600);
 
         this.storedConnections = new ComboBox<>();
         this.dialogs = new ComboBox<>();
@@ -74,7 +86,29 @@ public class DialogFillWizard extends AbstractWizard {
             this.dialogs.getItems().addAll(dictionary.getWindows().stream().map(Object::toString).collect(Collectors.toList()));
         });
 
+        dialogs.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
 
+        });
+
+        Button scan = new Button();
+        TreeView<String> treeView = new TreeView<>();
+
+        ColumnConstraints col1 = new ColumnConstraints(300, 300, 300, Priority.SOMETIMES, HPos.LEFT, true);
+        ColumnConstraints col2 = new ColumnConstraints(300, 300, 300, Priority.SOMETIMES, HPos.LEFT, true);
+
+        GridPane grid = new GridPane();
+        grid.setVgap(10);
+        grid.setHgap(10);
+        grid.getColumnConstraints().addAll(col1, col2);
+
+        grid.add(new Label("Select stored connection: "), 0, 0);
+        grid.add(storedConnections, 1, 0);
+        grid.add(new Label("Select dialog: "), 0, 1);
+        grid.add(dialogs, 1, 1);
+        grid.add(scan, 0, 2);
+        grid.add(treeView, 0, 3, 2, 1);
+
+        borderPane.setCenter(grid);
     }
 
 
