97c2ead5f8057f210d773e67df36368c2ed3a389
victor.krasnovid <victor.krasnovid@exactprosystems.com>
2017-08-09 15:19:13 +0400

    RM43773 intermediate update

--- core/src/com/exactprosystems/jf/tool/wizard/all/DialogFillWizard.java
+++ core/src/com/exactprosystems/jf/tool/wizard/all/DialogFillWizard.java
@@ -4,31 +4,45 @@ import com.exactprosystems.jf.actions.gui.DialogFill;
 import com.exactprosystems.jf.api.app.*;
 import com.exactprosystems.jf.api.common.IContext;
 
+import com.exactprosystems.jf.api.error.JFRemoteException;
 import com.exactprosystems.jf.api.wizard.WizardAttribute;
 import com.exactprosystems.jf.api.wizard.WizardCategory;
 import com.exactprosystems.jf.api.wizard.WizardCommand;
 import com.exactprosystems.jf.api.wizard.WizardManager;
-import com.exactprosystems.jf.documents.config.Context;
+import com.exactprosystems.jf.common.utils.XpathUtils;
 import com.exactprosystems.jf.documents.matrix.parser.Parameters;
 import com.exactprosystems.jf.documents.matrix.parser.Tokens;
 import com.exactprosystems.jf.documents.matrix.parser.items.MatrixItem;
 import com.exactprosystems.jf.documents.matrix.parser.items.TypeMandatory;
-import com.exactprosystems.jf.tool.ApplicationConnector;
 import com.exactprosystems.jf.tool.Common;
+import com.exactprosystems.jf.tool.custom.scaledimage.ImageViewWithScale;
 import com.exactprosystems.jf.tool.dictionary.DictionaryFx;
+import com.exactprosystems.jf.tool.helpers.DialogsHelper;
 import com.exactprosystems.jf.tool.matrix.MatrixFx;
 import com.exactprosystems.jf.tool.wizard.AbstractWizard;
 import com.exactprosystems.jf.tool.wizard.CommandBuilder;
+import com.exactprosystems.jf.tool.wizard.WizardMatcher;
+import com.exactprosystems.jf.tool.wizard.related.WizardHelper;
 import javafx.application.Platform;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.HPos;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.cell.CheckBoxListCell;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
+import org.w3c.dom.Document;
 
+import java.awt.*;
 import java.util.*;
 
+import java.util.List;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
@@ -51,11 +65,16 @@ public class DialogFillWizard extends AbstractWizard {
     private ComboBox<String> storedConnections;
     private ComboBox<String> dialogs;
     private String currentAdapterStore;
-    private ApplicationConnector appConnector;
     private Collection<IWindow> windows;
     private IWindow currentDialog;
     private MatrixItem currentItem;
     private ListView<Bean> beanListView;
+    private AppConnection appConnection;
+    private ImageViewWithScale imageViewWithScale;
+    private Document document;
+    private WizardMatcher matcher;
+    private ListView<ControlItem> textBoxes;
+
 
 
     @Override
@@ -63,13 +82,17 @@ public class DialogFillWizard extends AbstractWizard {
         super.init(context, wizardManager, parameters);
         this.currentMatrix = super.get(MatrixFx.class, parameters);
         this.dictionary = (DictionaryFx) this.currentMatrix.getDefaultApp().getDictionary();
-        this.appConnector = new ApplicationConnector(((Context) context).getFactory());
         this.windows = dictionary.getWindows();
         this.currentItem = get(MatrixItem.class, parameters);
+
     }
 
     @Override
     protected void initDialog(BorderPane borderPane) {
+
+        this.imageViewWithScale = new ImageViewWithScale();
+        this.textBoxes = new ListView<>();
+
 
         this.beanListView = new ListView<>();
         Button scan = new Button("Scan");
@@ -83,33 +106,38 @@ public class DialogFillWizard extends AbstractWizard {
             if (newValue != null)
             {
                 this.setCurrentAdapterStore(newValue);
-                Common.tryCatch(() -> this.connectToApplicationFromStore(this.currentAdapterStore), "Error on connect");
+                this.connectToApplicationFromStore(this.currentAdapterStore);
             }
             this.dialogs.getItems().clear();
             this.windows = dictionary.getWindows();
             this.dialogs.getItems().addAll(windows.stream().map(Object::toString).collect(Collectors.toList()));
         });
 
-        dialogs.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+        this.dialogs.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
             this.currentDialog = this.dictionary.getWindows().stream().filter(iWindow -> iWindow.getName().equals(newValue)).findFirst().get();
+            this.dialogs.valueProperty().set(newValue);
+            onDialogSelected();
         });
+        this.textBoxes.setCellFactory(CheckBoxListCell.forListView(ControlItem::onProperty));
 
-        ColumnConstraints col1 = new ColumnConstraints(200, 150, 300, Priority.SOMETIMES, HPos.LEFT, true);
-        ColumnConstraints col2 = new ColumnConstraints(200, 150, 300, Priority.SOMETIMES, HPos.LEFT, true);
+        ColumnConstraints col1 = new ColumnConstraints(300, 300, 300, Priority.SOMETIMES, HPos.LEFT, true);
+        ColumnConstraints col2 = new ColumnConstraints(300, 300, 300, Priority.SOMETIMES, HPos.CENTER, true);
+        ColumnConstraints col3 = new ColumnConstraints(300, 300, 300, Priority.SOMETIMES, HPos.CENTER, true);
 
         GridPane.setFillWidth(storedConnections, true);
         GridPane.setFillWidth(dialogs, true);
         grid.setVgap(10);
         grid.setHgap(10);
-        grid.getColumnConstraints().addAll(col1, col2);
+        grid.getColumnConstraints().addAll(col1, col2, col3);
 
         grid.add(new Label("Select stored connection: "), 0, 0);
         grid.add(storedConnections, 1, 0);
-        grid.add(new Label("Select dialog: "), 0, 1);
-        grid.add(dialogs, 1, 1);
-        grid.add(scan, 0, 2, 2, 1);
-        grid.add(this.beanListView, 0, 3, 2, 1);
-
+        grid.add(new Label("Select dialog: "), 2, 0);
+        grid.add(dialogs, 3, 0);
+        grid.add(this.textBoxes, 0, 1);
+        grid.add(scan, 0, 2);
+        grid.add(this.beanListView, 0, 3);
+        grid.add(this.imageViewWithScale, 1, 1, 3, 3);
         borderPane.setCenter(grid);
     }
 
@@ -137,10 +165,12 @@ public class DialogFillWizard extends AbstractWizard {
     @Override
     public boolean beforeRun() {
 
+
         return true;
     }
 
-    private void displayStores() throws Exception {
+    private void displayStores() throws Exception
+    {
         Map<String, Object> storeMap = this.dictionary.getFactory().getConfiguration().getStoreMap();
         Collection<String> stories = new ArrayList<>();
         if (!storeMap.isEmpty())
@@ -154,9 +184,9 @@ public class DialogFillWizard extends AbstractWizard {
                 }
             });
         }
-        String currentAdapterStore = storedConnections.getSelectionModel().getSelectedItem();
+        this.currentAdapterStore = storedConnections.getSelectionModel().getSelectedItem();
 
-        this.displayStoreActionControl(stories, currentAdapterStore);
+        this.displayStoreActionControl(stories, this.currentAdapterStore);
     }
 
     private void displayStoreActionControl(Collection<String> stories, String lastSelectedStore) {
@@ -170,36 +200,32 @@ public class DialogFillWizard extends AbstractWizard {
         });
     }
 
-    private void connectToApplicationFromStore(String idAppStore) throws Exception {
-        if (idAppStore == null || idAppStore.isEmpty())
+    private void connectToApplicationFromStore(String idAppStore)
+    {
+        if (idAppStore != null && !idAppStore.isEmpty())
         {
-            this.appConnector.setIdAppEntry(null);
-            this.appConnector.setAppConnection(null);
-        }
-        else
-        {
-            AppConnection appConnection = (AppConnection) this.dictionary.getFactory().getConfiguration().getStoreMap().get(idAppStore);
-            this.appConnector.setIdAppEntry(appConnection.getId());
-            this.appConnector.setAppConnection(appConnection);
+            this.appConnection = (AppConnection) this.dictionary.getFactory().getConfiguration().getStoreMap().get(idAppStore);
         }
     }
 
     private Map<String, String> getLocatorsValues(Collection<IControl> controls) {
 
-        IRemoteApplication service = this.appConnector.getAppConnection().getApplication().service();
+        IRemoteApplication service = this.appConnection.getApplication().service();
 
         Map<String, String> map = new HashMap<>();
         for (IControl o : controls)
         {
             map.put(o.getID(), Common.tryCatch(() -> String.valueOf(o.operate(service, this.currentDialog, Do.getValue())
-                    .getValue()), "Error on get values from controls", ""));
+                    .getValue()), "Error on get values from controls.", ""));
         }
         return map;
     }
 
     private void createBeans() {
 
-        Collection<IControl> textBoxes = currentDialog.getControls(IWindow.SectionKind.Run).stream().filter(iControl -> iControl.getBindedClass().equals(ControlKind.TextBox)).collect(Collectors.toList());
+        Collection<IControl> textBoxes = currentDialog.getControls(IWindow.SectionKind.Run).stream()
+                .filter(iControl -> iControl.getBindedClass().equals(ControlKind.TextBox)).collect(Collectors.toList());
+
         Map<String, String> values = getLocatorsValues(textBoxes);
         if (values != null)
         {
@@ -235,6 +261,72 @@ public class DialogFillWizard extends AbstractWizard {
             this.item = item;
             this.controlName = controlName;
             this.controlValue = controlValue;
+        }
+    }
+
+    private void onDialogSelected()
+    {
+        List<ControlItem> collect = currentDialog.getControls(IWindow.SectionKind.Run).stream()
+                .filter(iControl -> iControl.getBindedClass().equals(ControlKind.TextBox)).map(iControl -> new ControlItem(iControl,false)).collect(Collectors.toList());
+        ObservableList<ControlItem> objects = FXCollections.observableArrayList(collect);
+        this.textBoxes.getItems().clear();
+        this.textBoxes.getItems().addAll(objects);
+
+        IControl selfControl = Common.tryCatch(() -> this.currentDialog.getSelfControl(), "Error on get self", null);
+        WizardHelper.gainImageAndDocument(this.appConnection, selfControl, (image, doc) ->
+        {
+            this.imageViewWithScale.displayImage(image);
+
+            this.document = doc;
+            List<Rectangle> list = XpathUtils.collectAllRectangles(this.document);
+            this.imageViewWithScale.setListForSearch(list);
+            this.imageViewWithScale.setOnRectangleClick(rectangle -> {
+
+            });
+        }, ex ->
+        {
+            String message = ex.getMessage();
+            if (ex.getCause() instanceof JFRemoteException)
+            {
+                message = ((JFRemoteException) ex.getCause()).getErrorKind().toString();
+            }
+            DialogsHelper.showError(message);
+        });
+    }
+
+    private class ControlItem {
+        private IControl control;
+        private final BooleanProperty on = new SimpleBooleanProperty();
+
+        public IControl getControl() {
+            return this.control;
+        }
+
+        private ControlItem (IControl control, boolean on) {
+            this.control = control;
+            setOn(on);
+        }
+
+        public final String getName() {
+
+            return this.control.getID();
+        }
+
+        public final BooleanProperty onProperty() {
+            return this.on;
+        }
+
+        public final boolean isOn() {
+            return this.onProperty().get();
+        }
+
+        public final void setOn(final boolean on) {
+            this.onProperty().set(on);
+        }
+
+        @Override
+        public String toString() {
+            return getName();
         }
     }
 }
