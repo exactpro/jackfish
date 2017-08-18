547b92a522f5b8d8376ef11bb366cb3df6432514
andrey.bystrov <andrey.bystrov@exactprosystems.com>
2017-08-18 12:29:35 +0400

    RM #43933 JF hangs out after the report is finished

--- core/src/com/exactprosystems/jf/tool/matrix/MatrixFxController.java
+++ core/src/com/exactprosystems/jf/tool/matrix/MatrixFxController.java
@@ -502,24 +502,22 @@ public class MatrixFxController implements Initializable, ContainingParent, IMat
 
 	public void refresh()
 	{
-		Platform.runLater(() -> this.tree.refresh() );
+		this.tree.refresh();
 	}
 
 	public void refreshParameters(MatrixItem item, int selectIndex)
 	{
-		Platform.runLater(() -> this.tree.refreshParameters(item, selectIndex));
+		this.tree.refreshParameters(item, selectIndex);
 	}
 
 	public void setCurrent(MatrixItem item, boolean needExpand)
 	{
-		Platform.runLater(() -> {
-			TreeItem<MatrixItem> treeItem = this.tree.find(item);
-			if (treeItem == null)
-			{
-				treeItem = this.tree.find(this.tree.getRoot(), matrixItem -> item.getId().equals(matrixItem.getId()));
-			}
-			this.tree.setCurrent(treeItem, needExpand);
-		} );
+		TreeItem<MatrixItem> treeItem = this.tree.find(item);
+		if (treeItem == null)
+		{
+			treeItem = this.tree.find(this.tree.getRoot(), matrixItem -> item.getId().equals(matrixItem.getId()));
+		}
+		this.tree.setCurrent(treeItem, needExpand);
 	}
 
 	public void remove(MatrixItem item)
