10b8072876d4e3609f90b1a7f0f9cb44a0c0d1b9
andrey.smirnov <andrey.smirnov@exactprosystems.com>
2017-10-31 09:45:38 +0300

    base for new plugin javafx (AppFactoryFx)

--- plugins/app/javafx/src/com/exactprosystems/jf/app/AppFactoryFx.java
+++ plugins/app/javafx/src/com/exactprosystems/jf/app/AppFactoryFx.java
@@ -1,13 +1,20 @@
 package com.exactprosystems.jf.app;
 
-import com.exactprosystems.jf.api.app.AbstractApplicationFactory;
-import com.exactprosystems.jf.api.app.IApplication;
-import com.exactprosystems.jf.api.app.PluginInfo;
+import com.exactprosystems.jf.api.app.*;
 import com.exactprosystems.jf.api.common.ParametersKind;
 import com.exactprosystems.jf.api.common.PluginDescription;
+import com.exactprosystems.jf.api.common.PluginFieldDescription;
 import com.exactprosystems.jf.api.common.i18n.R;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import org.w3c.dom.Node;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @PluginDescription(
         pluginName = "JAVAFX",
@@ -16,11 +23,75 @@ import java.io.InputStream;
 )
 public class AppFactoryFx extends AbstractApplicationFactory
 {
+    public static final String helpFileName     = "help.txt";
+
+    @PluginFieldDescription(parameter = "LogLevel", description = R.JAVAFX_PLUGIN_LOG_LEVEL, example = "ALL")
+    public static final String logLevel         = "LogLevel";
+    @PluginFieldDescription(parameter = "jreExec", description = R.JAVAFX_PLUGIN_JRE_EXEC, example = "C:\\Program Files\\Java\\jdk1.8.0_XX\\bin\\java")
+    public static final String jreExecName 		= "jreExec";
+    @PluginFieldDescription(parameter = "jreArgs", description = R.JAVAFX_PLUGIN_JRE_ARGS, example = "-Xms128m -Xmx1G")
+    public static final String jreArgsName 		= "jreArgs";
+    @PluginFieldDescription(parameter = "MainClass", description = R.JAVAFX_PLUGIN_MAIN_CLASS, example = "com.example.MainClass")
+    public static final String mainClassName 	= "MainClass";
+    @PluginFieldDescription(parameter = "Jar", description = R.JAVAFX_PLUGIN_JAR, example = "'C:/example.jar'")
+    public static final String jarName 			= "Jar";
+    @PluginFieldDescription(parameter = "Args", description = R.JAVAFX_PLUGIN_ARGS, example = "'Arg1'")
+    public static final String argsName 		= "Args";
+
+    @PluginFieldDescription(parameter = "URL", description = R.JAVAFX_PLUGIN_URL, example = "'http://site.com/start.jnlp'")
+    public static final String urlName 			= "URL";
+
+    @PluginFieldDescription(parameter = "Title", description = R.JAVAFX_PLUGIN_TITLE, example = "'Title'")
+    public static final String propertyTitle    = "Title";
+
+    private static String[]    empty = {  };
+
+    private static PluginInfo  info;
+
+    static
+    {
+        Map<LocatorFieldKind, String> fieldMap = new HashMap<>();
+
+        fieldMap.put(LocatorFieldKind.UID,      null);
+        fieldMap.put(LocatorFieldKind.ACTION,   "action");
+        fieldMap.put(LocatorFieldKind.CLAZZ,    "class");
+        fieldMap.put(LocatorFieldKind.NAME,     "name");
+        fieldMap.put(LocatorFieldKind.TITLE,    "title");
+        fieldMap.put(LocatorFieldKind.TEXT,     null);
+        fieldMap.put(LocatorFieldKind.TOOLTIP,  "tooltip");
+
+        info = new JavaFxPluginInfo(fieldMap, new ArrayList<>());
+
+        info.addTypes(ControlKind.Any, "*");
+        info.addTypes(ControlKind.Button, Button.class.getSimpleName());
+        info.addTypes(ControlKind.CheckBox, CheckBox.class.getSimpleName());
+        info.addTypes(ControlKind.ComboBox, ComboBox.class.getSimpleName());
+        info.addTypes(ControlKind.Dialog, Dialog.class.getSimpleName());
+        info.addTypes(ControlKind.Frame, Stage.class.getSimpleName());
+        info.addTypes(ControlKind.Label, Label.class.getSimpleName());
+        info.addTypes(ControlKind.ListView, ListView.class.getSimpleName());
+        info.addTypes(ControlKind.Menu, Menu.class.getSimpleName());
+        info.addTypes(ControlKind.MenuItem, MenuItem.class.getSimpleName());
+        info.addTypes(ControlKind.Panel, Pane.class.getSimpleName());
+        info.addTypes(ControlKind.ProgressBar, ProgressBar.class.getSimpleName());
+        info.addTypes(ControlKind.RadioButton, RadioButton.class.getSimpleName());
+        info.addTypes(ControlKind.ScrollBar, ScrollBar.class.getSimpleName());
+        info.addTypes(ControlKind.Slider, Slider.class.getSimpleName());
+        info.addTypes(ControlKind.Splitter, SplitPane.class.getSimpleName());
+        info.addTypes(ControlKind.Spinner, Spinner.class.getSimpleName());
+        info.addTypes(ControlKind.Table, TableView.class.getSimpleName());
+        info.addTypes(ControlKind.TabPanel, TabPane.class.getSimpleName());
+        info.addTypes(ControlKind.TextBox, TextField.class.getSimpleName(), TextArea.class.getSimpleName());
+        info.addTypes(ControlKind.ToggleButton, ToggleButton.class.getSimpleName());
+        info.addTypes(ControlKind.Tooltip, Tooltip.class.getSimpleName());
+        info.addTypes(ControlKind.Tree, TreeView.class.getSimpleName());
+        info.addTypes(ControlKind.Wait,"*");
+    }
 
     @Override
     public InputStream getHelp()
     {
-        return null;
+        return AppFactoryFx.class.getResourceAsStream(helpFileName);
     }
 
     @Override
@@ -32,13 +103,13 @@ public class AppFactoryFx extends AbstractApplicationFactory
     @Override
     public String getRemoteClassName()
     {
-        return null;
+        return RemoteApplicationFx.class.getCanonicalName();
     }
 
     @Override
     public PluginInfo getInfo()
     {
-        return null;
+        return info;
     }
 
     @Override
@@ -56,6 +127,20 @@ public class AppFactoryFx extends AbstractApplicationFactory
     @Override
     public String[] listForParameter(String parameterToFill)
     {
-        return new String[0];
+        return empty;
+    }
+
+    private static class JavaFxPluginInfo extends PluginInfo
+    {
+        JavaFxPluginInfo(Map<LocatorFieldKind, String> fieldMap, List<String> notStableList)
+        {
+            super(fieldMap, notStableList);
+        }
+
+        @Override
+        public ControlKind derivedControlKindByNode(Node node)
+        {
+            return ControlKind.Any;
+        }
     }
 }
