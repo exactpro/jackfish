8dc7036cfa480be8c9614f1031a0f1cd9d19d822
andrey.bystrov <andrey.bystrov@exactprosystems.com>
2017-11-03 19:40:23 +0400

    Task #45907 Create new plugin [javafx]

--- plugins/app/javafx/src/com/exactprosystems/jf/app/FxOperationExecutor.java
+++ plugins/app/javafx/src/com/exactprosystems/jf/app/FxOperationExecutor.java
@@ -2,16 +2,17 @@ package com.exactprosystems.jf.app;
 
 import com.exactprosystems.jf.api.app.*;
 import com.exactprosystems.jf.api.client.ICondition;
-import javafx.scene.Node;
+import javafx.event.EventTarget;
 import org.apache.log4j.Logger;
 import org.w3c.dom.Document;
 
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Rectangle;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
-public class FxOperationExecutor extends AbstractOperationExecutor<Node>
+public class FxOperationExecutor extends AbstractOperationExecutor<EventTarget>
 {
     private Logger logger;
 
@@ -21,247 +22,237 @@ public class FxOperationExecutor extends AbstractOperationExecutor<Node>
         this.logger = logger;
     }
 
-    @Override
-    protected String getValueDerived(Node component) throws Exception
-    {
-        return null;
-    }
+	@Override
+	protected String getValueDerived(EventTarget component) throws Exception
+	{
+		return null;
+	}
 
-    @Override
-    protected List<String> getListDerived(Node component, boolean onlyVisible) throws Exception
-    {
-        return null;
-    }
+	@Override
+	protected List<String> getListDerived(EventTarget component, boolean onlyVisible) throws Exception
+	{
+		return null;
+	}
 
-    @Override
-    protected String getDerived(Node component) throws Exception
-    {
-        return null;
-    }
+	@Override
+	protected String getDerived(EventTarget component) throws Exception
+	{
+		return null;
+	}
 
-    @Override
-    protected String getAttrDerived(Node component, String name) throws Exception
-    {
-        return null;
-    }
+	@Override
+	protected String getAttrDerived(EventTarget component, String name) throws Exception
+	{
+		return null;
+	}
 
-    @Override
-    protected String scriptDerived(Node component, String script) throws Exception
-    {
-        return null;
-    }
+	@Override
+	protected String scriptDerived(EventTarget component, String script) throws Exception
+	{
+		return null;
+	}
 
-    @Override
-    protected String getValueTableCellDerived(Node component, int column, int row) throws Exception
-    {
-        return null;
-    }
+	@Override
+	protected String getValueTableCellDerived(EventTarget component, int column, int row) throws Exception
+	{
+		return null;
+	}
 
-    @Override
-    protected Map<String, String> getRowDerived(Node component, Locator additional, Locator header, boolean useNumericHeader, String[] columns, ICondition valueCondition, ICondition colorCondition) throws Exception
-    {
-        return null;
-    }
+	@Override
+	protected Map<String, String> getRowDerived(EventTarget component, Locator additional, Locator header, boolean useNumericHeader, String[] columns, ICondition valueCondition, ICondition colorCondition) throws Exception
+	{
+		return null;
+	}
 
-    @Override
-    protected Map <String, String> getRowByIndexDerived(Node component, Locator additional, Locator header, boolean useNumericHeader, String[] columns, int i) throws Exception
-    {
-        return null;
-    }
+	@Override
+	protected Map<String, String> getRowByIndexDerived(EventTarget component, Locator additional, Locator header, boolean useNumericHeader, String[] columns, int i) throws Exception
+	{
+		return null;
+	}
 
-    @Override
-    protected Map <String, ValueAndColor> getRowWithColorDerived(Node component, Locator additional, Locator header, boolean useNumericHeader, String[] columns, int i) throws Exception
-    {
-        return null;
-    }
+	@Override
+	protected Map<String, ValueAndColor> getRowWithColorDerived(EventTarget component, Locator additional, Locator header, boolean useNumericHeader, String[] columns, int i) throws Exception
+	{
+		return null;
+	}
 
-    @Override
-    protected String[][] getTableDerived(Node component, Locator additional, Locator header, boolean useNumericHeader, String[] columns) throws Exception
-    {
-        return new String[0][];
-    }
+	@Override
+	protected String[][] getTableDerived(EventTarget component, Locator additional, Locator header, boolean useNumericHeader, String[] columns) throws Exception
+	{
+		return new String[0][];
+	}
 
-    @Override
-    public Rectangle getRectangle(Node component) throws Exception
-    {
-        //todo check
-        try
-        {
-            return MatcherFx.getRect(component);
-        }
-        catch (Throwable e)
-        {
-            logger.error(String.format("getRectangle(%s)", component));
-            logger.error(e.getMessage(), e);
-            throw e;
-        }
-    }
+	@Override
+	public Rectangle getRectangle(EventTarget component) throws Exception
+	{
+		return null;
+	}
 
-    @Override
-    public Color getColor(Node component, boolean isForeground) throws Exception
-    {
-        return null;
-    }
+	@Override
+	public Color getColor(EventTarget component, boolean isForeground) throws Exception
+	{
+		return null;
+	}
 
-    @Override
-    public List <Node> findAll(ControlKind controlKind, Node window, Locator locator) throws Exception
-    {
-        return null;
-    }
+	@Override
+	public List<EventTarget> findAll(ControlKind controlKind, EventTarget window, Locator locator) throws Exception
+	{
+		return null;
+	}
 
-    @Override
-    public List <Node> findAll(Locator owner, Locator element) throws Exception
-    {
-        return null;
-    }
+	@Override
+	public List<EventTarget> findAll(Locator owner, Locator element) throws Exception
+	{
+		return null;
+	}
 
-    @Override
-    public Node find(Locator owner, Locator element) throws Exception
-    {
-        return null;
-    }
+	@Override
+	public EventTarget find(Locator owner, Locator element) throws Exception
+	{
+		return null;
+	}
 
-    @Override
-    public List <Node> findByXpath(Node element, String path) throws Exception
-    {
-        return null;
-    }
+	@Override
+	public List<EventTarget> findByXpath(EventTarget element, String path) throws Exception
+	{
+		return null;
+	}
 
-    @Override
-    public Node lookAtTable(Node table, Locator additional, Locator header, int x, int y) throws Exception
-    {
-        return null;
-    }
+	@Override
+	public EventTarget lookAtTable(EventTarget table, Locator additional, Locator header, int x, int y) throws Exception
+	{
+		return null;
+	}
 
-    @Override
-    public boolean elementIsEnabled(Node component) throws Exception
-    {
-        return !component.isDisabled();
-    }
+	@Override
+	public boolean elementIsEnabled(EventTarget component) throws Exception
+	{
+		return false;
+	}
 
-    @Override
-    public boolean elementIsVisible(Node component) throws Exception
-    {
-        return component.isVisible();
-    }
+	@Override
+	public boolean elementIsVisible(EventTarget component) throws Exception
+	{
+		return false;
+	}
 
-    @Override
-    public boolean tableIsContainer()
-    {
-        return false;
-    }
+	@Override
+	public boolean tableIsContainer()
+	{
+		return false;
+	}
 
-    @Override
-    public boolean mouse(Node component, int x, int y, MouseAction action) throws Exception
-    {
-        return false;
-    }
+	@Override
+	public boolean mouse(EventTarget component, int x, int y, MouseAction action) throws Exception
+	{
+		return false;
+	}
 
-    @Override
-    public boolean press(Node component, Keyboard key) throws Exception
-    {
-        return false;
-    }
+	@Override
+	public boolean press(EventTarget component, Keyboard key) throws Exception
+	{
+		return false;
+	}
 
-    @Override
-    public boolean upAndDown(Node component, Keyboard key, boolean b) throws Exception
-    {
-        return false;
-    }
+	@Override
+	public boolean upAndDown(EventTarget component, Keyboard key, boolean b) throws Exception
+	{
+		return false;
+	}
 
-    @Override
-    public boolean push(Node component) throws Exception
-    {
-        return false;
-    }
+	@Override
+	public boolean push(EventTarget component) throws Exception
+	{
+		return false;
+	}
 
-    @Override
-    public boolean toggle(Node component, boolean value) throws Exception
-    {
-        return false;
-    }
+	@Override
+	public boolean toggle(EventTarget component, boolean value) throws Exception
+	{
+		return false;
+	}
 
-    @Override
-    public boolean select(Node component, String selectedText) throws Exception
-    {
-        return false;
-    }
+	@Override
+	public boolean select(EventTarget component, String selectedText) throws Exception
+	{
+		return false;
+	}
 
-    @Override
-    public boolean selectByIndex(Node component, int index) throws Exception
-    {
-        return false;
-    }
+	@Override
+	public boolean selectByIndex(EventTarget component, int index) throws Exception
+	{
+		return false;
+	}
 
-    @Override
-    public boolean expand(Node component, String path, boolean expandOrCollapse) throws Exception
-    {
-        return false;
-    }
+	@Override
+	public boolean expand(EventTarget component, String path, boolean expandOrCollapse) throws Exception
+	{
+		return false;
+	}
 
-    @Override
-    public boolean text(Node component, String text, boolean clear) throws Exception
-    {
-        return false;
-    }
+	@Override
+	public boolean text(EventTarget component, String text, boolean clear) throws Exception
+	{
+		return false;
+	}
 
-    @Override
-    public boolean wait(Locator locator, int ms, boolean toAppear, AtomicLong atomicLong) throws Exception
-    {
-        return false;
-    }
+	@Override
+	public boolean wait(Locator locator, int ms, boolean toAppear, AtomicLong atomicLong) throws Exception
+	{
+		return false;
+	}
 
-    @Override
-    public boolean setValue(Node component, double value) throws Exception
-    {
-        return false;
-    }
+	@Override
+	public boolean setValue(EventTarget component, double value) throws Exception
+	{
+		return false;
+	}
 
-    @Override
-    public Document getTree(Node component) throws Exception
-    {
-        return null;
-    }
+	@Override
+	public Document getTree(EventTarget component) throws Exception
+	{
+		return null;
+	}
 
-    @Override
-    public boolean dragNdrop(Node drag, int x1, int y1, Node drop, int x2, int y2, boolean moveCursor) throws Exception
-    {
-        return false;
-    }
+	@Override
+	public boolean dragNdrop(EventTarget drag, int x1, int y1, EventTarget drop, int x2, int y2, boolean moveCursor) throws Exception
+	{
+		return false;
+	}
 
-    @Override
-    public boolean scrollTo(Node component, int index) throws Exception
-    {
-        return false;
-    }
+	@Override
+	public boolean scrollTo(EventTarget component, int index) throws Exception
+	{
+		return false;
+	}
 
-    @Override
-    public boolean mouseTable(Node component, int column, int row, MouseAction action) throws Exception
-    {
-        return false;
-    }
+	@Override
+	public boolean mouseTable(EventTarget component, int column, int row, MouseAction action) throws Exception
+	{
+		return false;
+	}
 
-    @Override
-    public boolean textTableCell(Node component, int column, int row, String text) throws Exception
-    {
-        return false;
-    }
+	@Override
+	public boolean textTableCell(EventTarget component, int column, int row, String text) throws Exception
+	{
+		return false;
+	}
 
-    @Override
-    public List <String> getRowIndexes(Node component, Locator additional, Locator header, boolean useNumericHeader, String[] columns, ICondition valueCondition, ICondition colorCondition) throws Exception
-    {
-        return null;
-    }
+	@Override
+	public List<String> getRowIndexes(EventTarget component, Locator additional, Locator header, boolean useNumericHeader, String[] columns, ICondition valueCondition, ICondition colorCondition) throws Exception
+	{
+		return null;
+	}
 
-    @Override
-    public int getTableSize(Node component, Locator additional, Locator header, boolean useNumericHeader) throws Exception
-    {
-        return 0;
-    }
+	@Override
+	public int getTableSize(EventTarget component, Locator additional, Locator header, boolean useNumericHeader) throws Exception
+	{
+		return 0;
+	}
 
-    @Override
-    public Color getColorXY(Node component, int x, int y) throws Exception
-    {
-        return null;
-    }
+	@Override
+	public Color getColorXY(EventTarget component, int x, int y) throws Exception
+	{
+		return null;
+	}
 }
--- plugins/app/javafx/src/com/exactprosystems/jf/app/FxRemoteApplication.java
+++ plugins/app/javafx/src/com/exactprosystems/jf/app/FxRemoteApplication.java
@@ -1,196 +1,343 @@
 package com.exactprosystems.jf.app;
 
 import com.exactprosystems.jf.api.app.*;
+import com.exactprosystems.jf.api.common.ProcessTools;
+import com.exactprosystems.jf.api.common.Str;
+import com.exactprosystems.jf.api.error.app.FeatureNotSupportedException;
+import com.exactprosystems.jf.api.error.app.NullParameterException;
 import org.apache.log4j.*;
 import org.w3c.dom.Document;
 
-import java.awt.*;
+import java.awt.Rectangle;
 import java.io.Serializable;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.rmi.RemoteException;
 import java.util.Collection;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Consumer;
 
 public class FxRemoteApplication extends RemoteApplication
 {
-    private Logger logger = null;
-    private Robot  currentRobot;
+	private Logger logger = null;
+	private FxOperationExecutor operationExecutor;
+	private PluginInfo          info;
 
-    private FxOperationExecutor operationExecutor;
-    private PluginInfo          info;
+	private boolean isInit = false;
+	private Thread thread;
 
-    @Override
-    protected void createLoggerDerived(String logName, String serverLogLevel, String serverLogPattern) throws Exception
-    {
-        try
-        {
-            this.logger = Logger.getLogger(FxRemoteApplication.class);
+	@Override
+	protected void createLoggerDerived(String logName, String serverLogLevel, String serverLogPattern) throws Exception
+	{
+		try
+		{
+			this.logger = Logger.getLogger(FxRemoteApplication.class);
 
-            Layout layout = new PatternLayout(serverLogPattern);
-            Appender appender = new FileAppender(layout, logName);
-            this.logger.addAppender(appender);
-            this.logger.setLevel(Level.toLevel(serverLogLevel, Level.ALL));
+			Layout layout = new PatternLayout(serverLogPattern);
+			Appender appender = new FileAppender(layout, logName);
+			this.logger.addAppender(appender);
+			this.logger.setLevel(Level.toLevel(serverLogLevel, Level.ALL));
 
-            MatcherFx.setLogger(this.logger);
-        }
-        catch (RemoteException e)
-        {
-            throw e;
-        }
-        catch (Exception e)
-        {
-            logger.error(String.format("createLoggerDerived(%s, %s,%s)", logName, serverLogLevel, serverLogPattern));
-            logger.error(e.getMessage(), e);
-            throw e;
-        }
-    }
+			MatcherFx.setLogger(this.logger);
+		}
+		catch (RemoteException e)
+		{
+			throw e;
+		}
+		catch (Exception e)
+		{
+			logger.error(String.format("createLoggerDerived(%s, %s,%s)", logName, serverLogLevel, serverLogPattern));
+			logger.error(e.getMessage(), e);
+			throw e;
+		}
+	}
 
-    @Override
-    protected void setPluginInfoDerived(PluginInfo info) throws Exception
-    {
+	@Override
+	public Serializable getProperty(String name, Serializable prop) throws RemoteException
+	{
+		//TODO implement
+		return null;
+	}
 
-    }
+	@Override
+	public void setProperty(String name, Serializable prop) throws RemoteException
+	{
 
-    @Override
-    protected int connectDerived(Map<String, String> args) throws Exception
-    {
-        return 0;
-    }
+	}
 
-    @Override
-    protected int runDerived(Map <String, String> args) throws Exception
-    {
-        return 0;
-    }
+	@Override
+	protected void setPluginInfoDerived(PluginInfo info) throws Exception
+	{
+		this.info = info;
+		this.operationExecutor.setPluginInfo(this.info);
+	}
 
-    @Override
-    protected void stopDerived(boolean needKill) throws Exception
-    {
+	@Override
+	protected int runDerived(Map <String, String> args) throws Exception
+	{
+		String mainClass = args.get(FxAppFactory.mainClassName);
+		if (Str.IsNullOrEmpty(mainClass))
+		{
+			throw new NullParameterException("MainClass can't be empty or null");
+		}
 
-    }
+		String jarName = args.get(FxAppFactory.jarName);
+		if (Str.IsNullOrEmpty(jarName))
+		{
+			throw new NullParameterException("Jar can't be empty or null");
+		}
 
-    @Override
-    protected void refreshDerived() throws Exception
-    {
+		String arguments = args.get(FxAppFactory.argsName);
+		logger.debug("Launching application: class=" + mainClass + " jar=" + jarName + " arg=" + arguments);
 
-    }
+		Exception[] last = new Exception[1];
+		AtomicBoolean isLoading = new AtomicBoolean(false);
 
-    @Override
-    protected String getAlertTextDerived() throws Exception
-    {
-        return null;
-    }
+		this.thread = new Thread(() -> {
+			try
+			{
+				ClassLoader classLoader = FxRemoteApplication.class.getClassLoader();
+				try(URLClassLoader urlClassLoader = URLClassLoader.newInstance(new URL[]{new URL("file://" + jarName)}, classLoader))
+				{
+					Class<?> applicationType = urlClassLoader.loadClass(mainClass);
+					Method mainMethod = applicationType.getMethod("main", String[].class);
+					this.operationExecutor = new FxOperationExecutor(this.useTrimText, this.logger);
+					this.isInit = true;
+					isLoading.set(true);
 
-    @Override
-    protected void navigateDerived(NavigateKind kind) throws Exception
-    {
+					mainMethod.invoke(null, new Object[]{arguments == null ? null : new String[]{arguments}});
+				}
+			}
+			catch (Exception e)
+			{
+				logger.error("connectDerived. keys : " + args.keySet() + " , value : " + args.values());
+				logger.error(e.getMessage(), e);
+				last[0] = e;
+				this.isInit = false;
+				isLoading.set(true);
+			}
+		});
+		this.thread.setName("Thread [" + this.thread.getId() + "] : start javafx application");
+		logger.debug("Thread " + this.thread + " started");
+		this.thread.start();
 
-    }
+		//wait while mainMethod not invoked
+		while (!isLoading.get())
+		{
+			Thread.sleep(1000);
+		}
 
-    @Override
-    protected void setAlertTextDerived(String text, PerformKind performKind) throws Exception
-    {
+		//check it we have exception - throw it
+		if (last[0] != null)
+		{
+			//we need throw exception and stop the thread
+			if (this.thread.isAlive())
+			{
+				this.thread.interrupt();
+				this.thread = null;
+			}
+			throw last[0];
+		}
 
-    }
+		return ProcessTools.currentProcessId();
+	}
 
-    @Override
-    protected Collection<String> titlesDerived() throws Exception
-    {
-        return null;
-    }
+	@Override
+	protected void stopDerived(boolean needKill) throws Exception
+	{
+		if (this.thread != null && this.thread.isAlive())
+		{
+			this.thread.interrupt();
+		}
+	}
 
-    @Override
-    protected String switchToDerived(Map <String, String> criteria, boolean softCondition) throws Exception
-    {
-        return null;
-    }
+	@Override
+	protected Collection<String> titlesDerived() throws Exception
+	{
+		//TODO implement
+		return null;
+	}
 
-    @Override
-    protected void switchToFrameDerived(Locator owner, Locator element) throws Exception
-    {
+	@Override
+	protected String switchToDerived(Map <String, String> criteria, boolean softCondition) throws Exception
+	{
+		//TODO implement
+		return null;
+	}
 
-    }
+	@Override
+	protected void resizeDerived(Resize resize, int height, int width) throws Exception
+	{
 
-    @Override
-    protected void resizeDerived(Resize resize, int height, int width) throws Exception
-    {
+	}
 
-    }
+	@Override
+	protected Collection <String> findAllDerived(Locator owner, Locator element) throws Exception
+	{
+		return null;
+	}
 
-    @Override
-    protected Collection <String> findAllDerived(Locator owner, Locator element) throws Exception
-    {
-        return null;
-    }
+	@Override
+	protected ImageWrapper getImageDerived(Locator owner, Locator element) throws Exception
+	{
+		return null;
+	}
 
-    @Override
-    protected ImageWrapper getImageDerived(Locator owner, Locator element) throws Exception
-    {
-        return null;
-    }
+	@Override
+	protected Rectangle getRectangleDerived(Locator owner, Locator element) throws Exception
+	{
+		return this.tryAndThrowOrReturn(
+				() ->
+				{
+					if (element == null)
+					{
+						//TODO need implement
+						return null;
+					}
+					else
+					{
+						return this.operationExecutor.getRectangle(this.operationExecutor.find(owner, element));
+					}
+				},
+				e ->
+				{
+					logger.error(String.format("getRectangleDerived(%s, %s)", owner, element));
+					logger.error(e.getMessage(), e);
+				}
+		);
+	}
 
-    @Override
-    protected Rectangle getRectangleDerived(Locator owner, Locator element) throws Exception
-    {
-        return null;
-    }
+	@Override
+	protected OperationResult operateDerived(Locator owner, Locator element, Locator rows, Locator header, Operation operation) throws Exception
+	{
+		return this.tryAndThrowOrReturn(
+				() -> operation.operate(this.operationExecutor, owner, element, rows, header),
+				e ->
+				{
+					logger.error(String.format("operateDerived(%s,%s,%s,%s,%s)", owner, element, rows, header, operation));
+					logger.error("EXCEPTION : " + e.getMessage(), e);
+				}
+		);
+	}
 
-    @Override
-    protected OperationResult operateDerived(Locator owner, Locator element, Locator rows, Locator header, Operation operation) throws Exception
-    {
-        return null;
-    }
+	@Override
+	protected CheckingLayoutResult checkLayoutDerived(Locator owner, Locator element, Spec spec) throws Exception
+	{
+		return this.tryAndThrowOrReturn(
+				() -> spec.perform(this.operationExecutor, owner, element),
+				e ->
+				{
+					logger.error(String.format("checkLayoutDerived(%s,%s,%s)", owner, element, spec));
+					logger.error(e.getMessage(), e);
+				}
+		);
+	}
 
-    @Override
-    protected CheckingLayoutResult checkLayoutDerived(Locator owner, Locator element, Spec spec) throws Exception
-    {
-        return null;
-    }
+	@Override
+	protected int closeAllDerived(Locator element, Collection <LocatorAndOperation> operations) throws Exception
+	{
+		return 0;
+	}
 
-    @Override
-    protected void newInstanceDerived(Map <String, String> args) throws Exception
-    {
+	@Override
+	protected Document getTreeDerived(Locator owner) throws Exception
+	{
+		return null;
+	}
 
-    }
+	@Override
+	protected void startNewDialogDerived() throws Exception
+	{
 
-    @Override
-    protected int closeAllDerived(Locator element, Collection <LocatorAndOperation> operations) throws Exception
-    {
-        return 0;
-    }
+	}
 
-    @Override
-    protected String closeWindowDerived() throws Exception
-    {
-        return null;
-    }
+	@Override
+	protected void moveWindowDerived(int x, int y) throws Exception
+	{
 
-    @Override
-    protected Document getTreeDerived(Locator owner) throws Exception
-    {
-        return null;
-    }
+	}
 
-    @Override
-    protected void startNewDialogDerived() throws Exception
-    {
+	//region FeatureNotSupported
+	@Override
+	protected int connectDerived(Map<String, String> args) throws Exception
+	{
+		logger.info("##########################################################################################################");
+		throw new FeatureNotSupportedException("Connect");
+	}
 
-    }
+	@Override
+	protected void refreshDerived() throws Exception
+	{
+		throw new FeatureNotSupportedException("refresh");
+	}
 
-    @Override
-    protected void moveWindowDerived(int x, int y) throws Exception
-    {
+	@Override
+	protected String getAlertTextDerived() throws Exception
+	{
+		throw new FeatureNotSupportedException("getAlertText");
+	}
 
-    }
+	@Override
+	protected void navigateDerived(NavigateKind kind) throws Exception
+	{
+		throw new FeatureNotSupportedException("navigate");
+	}
 
-    @Override
-    public Serializable getProperty(String name, Serializable prop) throws RemoteException
-    {
-        return null;
-    }
+	@Override
+	protected void setAlertTextDerived(String text, PerformKind performKind) throws Exception
+	{
+		throw new FeatureNotSupportedException("setAlertText");
+	}
 
-    @Override
-    public void setProperty(String name, Serializable prop) throws RemoteException
-    {
+	@Override
+	protected void switchToFrameDerived(Locator owner, Locator element) throws Exception
+	{
+		throw new FeatureNotSupportedException("switchToFrame");
+	}
 
-    }
+	@Override
+	protected void newInstanceDerived(Map <String, String> args) throws Exception
+	{
+		throw new FeatureNotSupportedException("newInstance");
+	}
+
+	@Override
+	protected String closeWindowDerived() throws Exception
+	{
+		throw new FeatureNotSupportedException("closeWindow()");
+	}
+	//endregion
+
+	//region private methods
+
+	private <T> T tryAndThrowOrReturn(IReturn<T> func, Consumer<Exception> log) throws Exception
+	{
+		if (!this.isInit)
+		{
+			throw new Exception("Application is not init");
+		}
+		try
+		{
+			return func.call();
+		}
+		catch (RemoteException e)
+		{
+			throw e;
+		}
+		catch (Exception e)
+		{
+			log.accept(e);
+			throw e;
+		}
+
+	}
+
+	private interface IReturn<T>
+	{
+		T call() throws Exception;
+	}
+
+	//endregion
 }
