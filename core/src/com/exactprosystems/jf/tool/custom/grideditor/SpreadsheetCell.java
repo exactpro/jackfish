91462f28faada2f8ee759a5650aea8129f33233e
victor.krasnovid <victor.krasnovid@exactprosystems.com>
2017-11-15 15:19:46 +0400

    Task #46243 [JF] add copyright for all files

--- core/src/com/exactprosystems/jf/tool/custom/grideditor/SpreadsheetCell.java
+++ core/src/com/exactprosystems/jf/tool/custom/grideditor/SpreadsheetCell.java
@@ -1,9 +1,10 @@
 ////////////////////////////////////////////////////////////////////////////////
-//  Copyright (c) 2009-2015, Exactpro Systems, LLC
-//  Quality Assurance & Related Development for Innovative Trading Systems.
-//  All rights reserved.
-//  This is unpublished, licensed software, confidential and proprietary
-//  information which is the property of Exactpro Systems, LLC or its licensors.
+// Copyright (c) 2009-2017, Exactpro Systems
+// Quality Assurance & Related Software Development for Innovative Trading Systems.
+// London Stock Exchange Group.
+// All rights reserved.
+// This is unpublished, licensed software, confidential and proprietary
+// information which is the property of Exactpro Systems or its licensors.
 ////////////////////////////////////////////////////////////////////////////////
 package com.exactprosystems.jf.tool.custom.grideditor;
 
@@ -13,7 +14,6 @@ import javafx.beans.property.StringProperty;
 import javafx.collections.ObservableSet;
 import javafx.event.EventType;
 
-//TODO remove this interface
 public interface SpreadsheetCell
 {
 	EventType EDITABLE_EVENT_TYPE = new EventType("EditableEventType"); //$NON-NLS-1$
--- core/src/com/exactprosystems/jf/tool/documents/vars/SystemVarsFxController.java
+++ core/src/com/exactprosystems/jf/tool/documents/vars/SystemVarsFxController.java
@@ -1,9 +1,10 @@
 ////////////////////////////////////////////////////////////////////////////////
-//  Copyright (c) 2009-2015, Exactpro Systems, LLC
-//  Quality Assurance & Related Development for Innovative Trading Systems.
-//  All rights reserved.
-//  This is unpublished, licensed software, confidential and proprietary
-//  information which is the property of Exactpro Systems, LLC or its licensors.
+// Copyright (c) 2009-2017, Exactpro Systems
+// Quality Assurance & Related Software Development for Innovative Trading Systems.
+// London Stock Exchange Group.
+// All rights reserved.
+// This is unpublished, licensed software, confidential and proprietary
+// information which is the property of Exactpro Systems or its licensors.
 ////////////////////////////////////////////////////////////////////////////////
 
 package com.exactprosystems.jf.tool.documents.vars;
