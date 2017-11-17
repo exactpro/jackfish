a75c9b3897adcce1d694b800e06d02edab3f62ca
andrey.bystrov <andrey.bystrov@exactprosystems.com>
2017-11-17 11:06:53 +0400

    Task #46243 [JF] add copyright for all files add for win mock

--- mocks/win/mock_win/Form2.cs
+++ mocks/win/mock_win/Form2.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2009-2017, Exactpro Systems
+// Quality Assurance & Related Software Development for Innovative Trading Systems.
+// London Stock Exchange Group.
+// All rights reserved.
+// This is unpublished, licensed software, confidential and proprietary
+// information which is the property of Exactpro Systems or its licensors.
+////////////////////////////////////////////////////////////////////////////////
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
