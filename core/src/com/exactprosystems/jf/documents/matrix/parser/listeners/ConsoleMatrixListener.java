50b7132848150d428b2636b9b972bf04ebbf19ac
andrey.bystrov <andrey.bystrov@exactprosystems.com>
2017-12-20 15:39:46 +0400

    RM #42576 global code review matrix/parser/listeners

--- core/src/com/exactprosystems/jf/documents/matrix/parser/listeners/ConsoleMatrixListener.java
+++ core/src/com/exactprosystems/jf/documents/matrix/parser/listeners/ConsoleMatrixListener.java
@@ -15,34 +15,32 @@ import com.exactprosystems.jf.documents.matrix.parser.items.MatrixItem;
 
 public class ConsoleMatrixListener extends ConsoleErrorMatrixListener
 {
+	private final boolean showShortPaths;
+
 	public ConsoleMatrixListener(boolean showShortPaths)
 	{
 		super();
-		
 		this.showShortPaths = showShortPaths;
 	}
-	
+
 	@Override
 	public void matrixStarted(Matrix matrix)
 	{
 		super.matrixStarted(matrix);
-        System.out.println(String.format("Matrix '%s' started...", matrix.getNameProperty()));
+		System.out.println(String.format("Matrix '%s' started...", matrix.getNameProperty()));
 	}
 
 	@Override
 	public void matrixFinished(Matrix matrix, int passed, int failed)
 	{
 		super.matrixFinished(matrix, passed, failed);
-        System.out.println(String.format("Matrix '%s' finished.      PASSED: %d FAILED: %d", matrix.getNameProperty(), passed, failed));
+		System.out.println(String.format("Matrix '%s' finished.      PASSED: %d FAILED: %d", matrix.getNameProperty(), passed, failed));
 	}
 
 	@Override
 	public void finished(Matrix matrix, MatrixItem action, Result result)
-	{	
+	{
 		super.finished(matrix, action, result);
-        System.out.println(String.format("%s[%3d]  %-80s  %S", matrix.getNameProperty(), action.getNumber(), (this.showShortPaths ? action.getItemName() : action.getPath()), result));
+		System.out.println(String.format("%s[%3d]  %-80s  %S", matrix.getNameProperty(), action.getNumber(), (this.showShortPaths ? action.getItemName() : action.getPath()), result));
 	}
-	
-	private boolean showShortPaths = false;
-	
 }
--- core/src/com/exactprosystems/jf/documents/matrix/parser/listeners/IMatrixListener.java
+++ core/src/com/exactprosystems/jf/documents/matrix/parser/listeners/IMatrixListener.java
@@ -13,16 +13,64 @@ import com.exactprosystems.jf.documents.matrix.Matrix;
 import com.exactprosystems.jf.documents.matrix.parser.Result;
 import com.exactprosystems.jf.documents.matrix.parser.items.MatrixItem;
 
+/**
+ * The interface for matrix listener
+ */
 public interface IMatrixListener extends Cloneable
 {
-	void		reset				(Matrix matrix);
-	void		matrixStarted		(Matrix matrix);
-	void		matrixFinished		(Matrix matrix, int passed, int failed);
-	void		started				(Matrix matrix, MatrixItem item);
-	void		paused				(Matrix matrix, MatrixItem item);
-	void		finished			(Matrix matrix, MatrixItem item, Result result);
-	void		error				(Matrix matrix, int lineNumber, MatrixItem item, String message);
-	String		getExceptionMessage	();
-	boolean		isOk				();
-
+	/**
+	 * Reset the listener. It's mean, that methods {@link IMatrixListener#isOk()} after call this method will return true
+	 */
+	void reset(Matrix matrix);
+	/**
+	 * Notify, that the passed matrix started
+	 */
+	void matrixStarted(Matrix matrix);
+	/**
+	 * Notify, that passed matrix finished. Parameters passed and failed indicate count of passed and failed children of MatrixRoot ( usually it is TestCase)
+	 *
+	 * @param matrix the finished matrix
+	 * @param passed count of passed TestCase
+	 * @param failed count of failed TestCase
+	 */
+	void matrixFinished(Matrix matrix, int passed, int failed);
+	/**
+	 * Notify, that the item from the matrix start executing
+	 *
+	 * @param matrix the matrix
+	 * @param item the item, which is started
+	 */
+	void started(Matrix matrix, MatrixItem item);
+	/**
+	 * Notify, that the item from the matrix is paused ( usually on breakpoint)
+	 *
+	 * @param matrix the matrix
+	 * @param item the paused item
+	 */
+	void paused(Matrix matrix, MatrixItem item);
+	/**
+	 * Notify, that the item from the matrix is finished
+	 *
+	 * @param matrix the matrix
+	 * @param item the finished item
+	 * @param result the result of executing the item
+	 */
+	void finished(Matrix matrix, MatrixItem item, Result result);
+	/**
+	 * Notify error on the matrix
+	 *
+	 * @param matrix the matrix
+	 * @param lineNumber the line number of error the item
+	 * @param item the error item. It may be {@code null}
+	 * @param message the message of error
+	 */
+	void error(Matrix matrix, int lineNumber, MatrixItem item, String message);
+	/**
+	 * @return the last exception
+	 */
+	String getExceptionMessage();
+	/**
+	 * @return true, if matrix has no errors
+	 */
+	boolean isOk();
 }
