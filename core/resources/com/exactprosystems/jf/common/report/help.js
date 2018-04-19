bff50bc073bc78d32f6845a640d092742b9404e9
dmitry.guriev <dmitry.guriev@exactprosystems.com>
2018-04-19 13:28:16 +0300

    Add core/resources folder

---
+++ core/resources/com/exactprosystems/jf/common/report/help.js
@@ -0,0 +1,249 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2009-2018, Exactpro Systems LLC
+// www.exactpro.com
+// Build Software to Test Software
+//
+// All rights reserved.
+// This is unpublished, licensed software, confidential and proprietary
+// information which is the property of Exactpro Systems LLC or its licensors.
+////////////////////////////////////////////////////////////////////////////////
+var mame = $('.mainMenu');
+
+
+function doMenu(obj) {
+
+	var hashTag = obj.attr('id') + '_child';
+
+	obj.on('click', function () {
+
+		$(document.getElementById(hashTag)).toggle();
+
+		return false;
+
+	});
+
+}
+
+function doLoad(obj) {
+
+	obj.on('click', function () {
+
+		$('.mainMenu .active').removeClass('active');
+
+		obj.addClass('active');
+
+	});
+
+}
+
+
+$(document).ready(function () {
+
+	$.each($('.mParent'), function (i, val) {
+
+		doMenu($(val));
+
+	});
+
+	$.each($('li[role="presentation"][class!="mParent"]'), function (i, val) {
+
+		doLoad($(val));
+
+	});
+
+	mame.width($('.menuCont').width());
+
+	mame.height($(window).height() - mame[0].getBoundingClientRect().top);
+
+	$.each($("a[href^='#']"),function(i,val) {
+
+		var hr = $(val).attr('href');
+
+		var id = hr.substring(1);
+
+		if (id !== "") {
+
+			$(val).click(function(event) {
+
+				$('html, body').animate({
+
+					scrollTop: $(document.getElementById($(val).attr("href").substring(1))).offset().top-50
+
+				}, 500);
+
+			})
+
+		}
+
+	});
+	f();
+});
+
+function f(){
+var str = '';
+var currentIndex = 0;
+var allIns = [];
+var foundDivs = [];
+var scrollToDiv = function(div) {
+	$('html, body').animate({
+		scrollTop: $(div).offset().top-50
+	}, 500);
+}
+
+var scrollToElem = function(elem) {
+	$('html, body').animate({
+		scrollTop: $(elem).offset().top-50
+	}, 100);
+}
+
+jQuery.expr[":"].Contains = jQuery.expr.createPseudo(function(arg) {
+    return function( elem ) {
+		return jQuery(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
+    };
+});
+
+
+//go forward
+function goF() {
+    if(foundDivs.length === 0){return}
+    $(allIns[currentIndex]).toggleClass('currentFoundElement foundElement');
+    if(currentIndex+1 === allIns.length){
+        currentIndex = 0;
+        scrollToElem(allIns[currentIndex]);
+    }else{
+        scrollToElem(allIns[++ currentIndex])
+    }
+    $('.searchLabel').text(1 + currentIndex +' of '+ allIns.length);
+    $(allIns[currentIndex]).toggleClass('currentFoundElement foundElement');
+}
+
+//go back
+function goB() {
+    if(foundDivs.length === 0){return}
+    $(allIns[currentIndex]).toggleClass('currentFoundElement foundElement');
+    if(currentIndex === 0){
+        currentIndex = allIns.length-1;
+        scrollToElem(allIns[currentIndex]);
+    }else{
+        scrollToElem(allIns[--currentIndex]);
+    }
+    $('.searchLabel').text(1+currentIndex +' of '+ allIns.length);
+    $(allIns[currentIndex]).toggleClass('currentFoundElement foundElement');
+}
+
+function markAllElements(array, str) {
+    var re = new RegExp('(' + str + ')', 'ig');
+    $(array).each(function(i,elem){
+        elem.innerHTML = this.innerHTML.replace(re, '<ins>$1</ins>');
+    });
+    allIns = $('ins');
+    $('.searchLabel').text('1 of '+allIns.length);
+    scrollToElem(allIns[0]);
+    currentIndex++;
+    $(allIns[0]).toggleClass('currentFoundElement');
+
+}
+
+function clear() {
+
+    currentIndex = -1;
+    $('ins').contents().unwrap();
+    foundDivs = [];
+    allIns = [];
+    $('.searchLabel').text('');
+    $("#btnPrev").toggleClass('activeBtn nonActiveBtn');
+    $("#btnNext").toggleClass('activeBtn nonActiveBtn');
+}
+
+
+$(".searchInput").on("keypress paste", function(e) {
+
+if(e.which == 13) {
+
+	if( $(this).val() === str){
+	    goF();
+	}else{
+
+    clear();
+    str = $(this).val();
+    if(str.includes(' | ')){str = str.replace('|','\\|')}
+    if(str.includes(' ^ ')){str = str.replace('^','\\^')}
+
+    if(str.length === 0){
+        clear();
+        return;
+    }
+
+    $("#btnPrev").toggleClass('activeBtn nonActiveBtn');
+    $("#btnNext").toggleClass('activeBtn nonActiveBtn');
+
+    if($(this).val().includes('|') || $(this).val().includes('^')){
+    	foundDivs = $('h3:contains('+str+'), h2:contains('+str+'), p:contains('+str+'), td:contains('+str+'), font:contains('+str+'), pre:contains('+str+')');
+    }else{
+    	foundDivs = $('h3:contains('+str+'), h2:Contains('+str+'), p:Contains('+str+'), td:Contains('+str+'), font:Contains('+str+'), pre:Contains('+str+')');
+    }
+
+    if(foundDivs.length === 0){
+        $('.searchLabel').text(0);
+        return;
+    }
+    markAllElements(foundDivs, str);
+    }
+
+    }
+});
+
+$("#btnNext").click(function(e){goF();});
+
+$("#btnPrev").click(function(e){goB();});
+}
+
+$(window).resize(function () {
+
+	mame.width($('.menuCont').width());
+
+	$('.menuCont').height(mame.height());
+
+	mame.height($(window).height() - mame[0].getBoundingClientRect().top);
+
+});
+
+
+function DoubleScroll(element) {
+
+	var scrollbar = document.createElement('div');
+
+	scrollbar.appendChild(document.createElement('div'));
+
+	scrollbar.style.overflow = 'auto';
+
+	scrollbar.style.overflowY = 'hidden';
+
+	scrollbar.className += " dobscrollDiv";
+
+	scrollbar.firstChild.style.width = element.scrollWidth + 'px';
+
+	//scrollbar.firstChild.style.paddingTop= '1px';
+
+	scrollbar.firstChild.appendChild(document.createTextNode('\xA0'));
+
+	scrollbar.onscroll = function () {
+
+		element.scrollLeft = scrollbar.scrollLeft;
+
+	};
+
+	element.onscroll = function () {
+
+		scrollbar.scrollLeft = element.scrollLeft;
+
+	};
+
+	element.parentNode.insertBefore(scrollbar, element);
+
+}
+
+
+DoubleScroll(document.getElementById('doublescroll'));
+
+$('#co-1Table').height($('.dobscrollDiv').height());
---
+++ core/resources/com/exactprosystems/jf/common/report/reports.js
@@ -0,0 +1,275 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2009-2018, Exactpro Systems LLC
+// www.exactpro.com
+// Build Software to Test Software
+//
+// All rights reserved.
+// This is unpublished, licensed software, confidential and proprietary
+// information which is the property of Exactpro Systems LLC or its licensors.
+////////////////////////////////////////////////////////////////////////////////
+$(document).ready(function(){
+	var comment = function(arr, func) {
+		arr.map(function(index, element) {
+			return $(element)
+		})
+		.filter(function(index, element) {
+			return element.hasClass('comment')
+		})
+		.each(function(index, element) {
+			func(element);
+		})
+	}
+	var hideComment = function(arr) {
+		comment(arr, function(e) {
+			e.hide();
+		})
+	}
+	var showComment = function(arr) {
+		comment(arr, function(e) {
+			e.show();
+		})
+	}
+
+	var hideTable = function() {
+		$(".repLog > tbody > tr.danger").hide();
+		$(".repLog > tbody > tr.success").hide();
+
+		$(".repLog tr[style*='table-row']").hide();
+
+		hideComment($(".repLog > tbody > tr.danger").prev())
+		hideComment($(".repLog > tbody > tr.success").prev())
+	}
+
+	var animateScrollTo = function() {
+		if (!$("table.repLog").is(":visible")) {
+			$('html, body').animate({
+				scrollTop: $("table.repLog").offset().top
+			}, 500);
+		}
+	}
+
+	$("tr.matrixSource").hide();
+
+	//hide main table
+	hideTable();
+
+    // hide all inner actions
+    $("a.showBody").parent().parent().next().hide();
+	$("a.showChapter").next().hide();
+
+	//function for move reports/images/charts/etc
+	$("div.movable").each(function(index) {
+		var me = $(this);
+        $("#TC_" + ('' + me.data("moveto")).replace(" ", "\\ "))[0].appendChild(me[0]);
+		me.attr('class','moved');
+	})
+
+	//show matrix source
+	$("a.showSource").toggle(
+		function(event) {
+			$("tr.matrixSource").show();
+			event.preventDefault();
+		},
+		function(event) {
+			$("tr.matrixSource").hide();
+			event.preventDefault();
+		}
+	);
+
+	$("button.filterTotal").click(
+		function(event) {
+			hideTable();
+			$(".repLog > tbody > tr.danger").show();
+			$(".repLog > tbody > tr.success").show();
+
+			showComment($(".repLog > tbody > tr.danger").prev())
+			showComment($(".repLog > tbody > tr.success").prev())
+
+			$('.filterPassed, .filterFailed').removeClass('active');
+
+			animateScrollTo();
+
+			event.preventDefault();
+		}
+	);
+	$("button.filterPassed").click(
+		function(event) {
+			hideTable();
+
+			$(".repLog > tbody > tr.success").show();
+			showComment($(".repLog > tbody > tr.success").prev());
+
+			$('.filterFailed').removeClass('active');
+			$(".filterPassed").addClass('active');
+
+			animateScrollTo();
+
+			event.preventDefault();
+		}
+	);
+	$("button.filterFailed").click(
+		function(event) {
+			hideTable();
+
+			$(".repLog > tbody > tr.danger").show();
+			showComment($(".repLog > tbody > tr.danger").prev());
+
+			animateScrollTo();
+
+			$('.filterPassed').removeClass('active');
+			$(".filterFailed").addClass('active');
+
+			event.preventDefault();
+		}
+	);
+
+	$("button.filterExpandAllFailed").click(function(event) {
+		var failedTr = $("tr.danger");
+
+		//show parents
+		var parent = failedTr.parent();
+		while ($(parent).length != 0 && !parent.is(":visible"))
+		{
+			parent.show();
+			parent = parent.parent();
+		}
+
+		failedTr.show();
+		failedTr.next().show();
+	});
+	$("button.filterCollapseAll").click(function(event) {
+		hideTable();
+
+	});
+
+	//timestamp
+	$('button.timestamp').toggle(
+		function(event) {
+			//show columns
+			var tdTS = $('td.timestamp');
+			$('td.timestamp').show()
+			$('td.timestamp').width('215')
+		},
+		function(event) {
+			//hide columns
+			$('td.timestamp').hide()
+			$('td.timestamp').width('0')
+		}
+	)
+	$('td.timestamp').hide();
+
+	var expandIfNeed = function(e) {
+		var trs = $(e).children().children().children().children();
+
+		var l = $(trs).length
+		if (l == 1) {
+			//this means, that element is left.
+			return;
+		} else if (l == 2 || l ==3 ) {
+			var hiddenPart = $(trs).parent().children('tr:not([class])');
+			//show hidden path and go deep
+			$(hiddenPart).show();
+			expandIfNeed(hiddenPart);
+		} else {
+			return;
+		}
+	}
+
+	$("a.showBody").click(function(event) {
+		var tbl = $(this).parent().parent().next();
+		if (tbl.is(":visible")) {
+			tbl.hide();
+		} else {
+			tbl.show();
+			expandIfNeed(tbl);
+			//find rotate div and rotate it
+			var childs = tbl.find(".rotatedDiv")
+			if (childs.length == 1) {
+				var rotateDiv = $(childs[0]).children('.rotate');
+				rotateDivFunction(rotateDiv)
+			}
+		}
+	});
+
+	$("a.showChapter").toggle(
+			function(event) {
+				$(this).next().show();
+				event.preventDefault();
+			}, 
+			function(event) {
+				$(this).next().hide();
+				event.preventDefault();
+			}
+		);
+	$('[indent-level]').filter(function(i,e) {
+		var indentLevel = $(e).attr('indent-level');
+
+		//update style
+		$(e).css('margin-left', function(i,e) {
+			return indentLevel*20;
+		});
+
+		return indentLevel != 0;
+	}).map(function(i,e) {
+		$(this).parent().parent().hide()
+
+	});
+
+	$('a.group').each(function(i,e) {
+		$(e).click(function(event) {
+
+			var tr = $(this).parent().parent();
+			var isOpened = $(tr).attr('opened');
+
+			if (isOpened == undefined) {
+				$(tr).attr('opened', 'true');
+				isOpened = true;
+			} else {
+				$(tr).removeAttr('opened');
+				isOpened = false;
+			}
+
+			var indentLevel = parseInt($(this).attr('indent-level'));
+			console.log(indentLevel);
+			var all = $(tr).nextAll();
+			for(var i = 0; i < all.length; i++) {
+				var currentTr = $(all[i]);
+				var elem = $(currentTr).find('.group');
+				var il = elem.attr('indent-level');
+				if (parseInt(il) <= indentLevel) {
+					break;
+				}
+				if (!isOpened) {
+					$(currentTr).removeAttr('opened');
+					$(currentTr).hide("fast");
+					continue;
+				}
+				if (parseInt(il) > (indentLevel+1)) {
+					continue;
+				}
+
+				if ($(currentTr).is(':visible')) {
+					$(currentTr).hide("fast");
+				} else {
+					$(currentTr).show("fast");
+				}
+			}
+		});
+
+	});
+
+	//rotate
+	var rotateDivFunction = function(rd) {
+		$(rd).width($(rd).children('span').width())
+		$(rd).parent().height($(rd).width())
+		$(rd).css("position", "relative")
+		$(rd).css("left", - ($(rd).width() / 2) + 10)
+		$(rd).css("top", ($(rd).width() / 2) - 10)
+	}
+	$('.rotate').each(function(i,e) {
+		rotateDivFunction(e);
+
+	})
+
+
+});
